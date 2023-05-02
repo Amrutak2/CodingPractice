c86291aad1fc7fa2f79bbf482ab3579dc816cf82
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-05-02 17:49:39 +0530

    Program1 2nd May 2023

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CodingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+    
+        string a, b;
+        Console.WriteLine("Enter two string: ");
+        a = Convert.ToString(Console.ReadLine());
+        char[] array = a.ToCharArray();
+       char[] arr;
+       Array.Reverse(array);
+       arr = array;
+        b = Convert.ToString(Console.ReadLine());
+         char[] array2 = b.ToCharArray();
+       Array.Reverse(array2);
+       
+        if(arr == array)
+        {
+            Console.WriteLine("Reverse");
+        }
+       else{
+           
+       
+        Console.WriteLine("The string are: {0} {1}" ,a, b);
+       }
+    
+   
+    
+        }
+    }
+}
