0881078aab24f483e391642bf8c963a1befd8581
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-05-11 17:01:09 +0530

    Program-5

---
+++ Program5/Program.cs
@@ -0,0 +1,33 @@
+﻿//5. Find the unique Character in the String
+
+using System;
+
+namespace Program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a;
+            Console.WriteLine("Enter a string: ");
+            a = Console.ReadLine();
+            char[] array = a.ToCharArray();
+            char[] array1 = new char[array.Length];
+            int c =0;
+            for(int i = 0; i<a.Length; i++){
+                if(!Char.IsLetterOrDigit(array[i])){
+                    array1[c] = a[i];
+                    c++;
+
+                }
+            }
+            Array.Resize(ref array1, c);
+            Console.WriteLine("Unique Characters: ");
+            foreach (var item in array1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
