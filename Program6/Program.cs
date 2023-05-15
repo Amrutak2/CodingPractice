79176d1236ddd9ba83f9db32b3057eec04ba1abe
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-05-15 16:21:12 +0530

    Program-6

---
+++ Program6/Program.cs
@@ -0,0 +1,34 @@
+﻿//6. Print the repeated characters in a String
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Enter a string: ");
+            a = Console.ReadLine();
+            var repeat = new List<char>();
+            // List<char> repeat = new List<char>();
+            foreach (var item in a)
+            {
+                int count = 0;
+                foreach (var chars in a)
+                {
+                    if(item == chars){
+                        count ++;
+                    }
+                }
+                if(count > 1 && !repeat.Contains(item)){
+                    repeat.Add(item);
+                }
+            }
+            Console.WriteLine( string.Join(", ", repeat));
+        }
+    }
+}
