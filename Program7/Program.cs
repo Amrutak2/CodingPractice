1194cac5715234783635539230d17078a19f44bb
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-05-15 16:21:42 +0530

    Program-7

---
+++ Program7/Program.cs
@@ -0,0 +1,28 @@
+﻿//7. How many times a particular character is repeated in a string
+
+using System;
+
+namespace Program7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string a;
+           Console.WriteLine("Enter a string: ");
+           a = Console.ReadLine();
+           while(a.Length > 0){
+            Console.Write(a[0] + ":");
+            int count = 0;
+            for(int j=0; j<a.Length; j++){
+                if(a[0] == a[j]){
+                    count ++;
+                }
+            }
+            Console.WriteLine(count);
+            a = a.Replace(a[0].ToString(), string.Empty);
+           }
+           Console.ReadLine();
+        }
+    }
+}
